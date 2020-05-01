6b2ce762d88a94e1380b0757d36d7072650c00b9
Q8i505 <64624332+Q8i505@users.noreply.github.com>
2020-05-01 19:27:56 +0300

    nothing

---
+++ MyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyApp
+//
+//  Created by M. AlQahtani on 5/1/20.
+//  Copyright © 2020 M.AlQahtani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hey, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
