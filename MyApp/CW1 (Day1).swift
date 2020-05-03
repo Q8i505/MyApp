cec3ef6f7e787d0fde09d532bc2817c95146e85a
Q8i505 <64624332+Q8i505@users.noreply.github.com>
2020-05-03 07:08:51 +0300

    CWI(Day1)

---
+++ MyApp/CW1 (Day1).swift
@@ -0,0 +1,9 @@
+//
+//  CW1 (Day1).swift
+//  MyApp
+//
+//  Created by M. AlQahtani on 5/3/20.
+//  Copyright © 2020 M.AlQahtani. All rights reserved.
+//
+
+import Foundation
--- MyApp/ContentView.swift
+++ MyApp/ContentView.swift
@@ -9,10 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    var athkar = ["استغفرالله", "الحمدلله", "الله اكبر"]
+    @State var thker = "الحمدلله"
     var body: some View {
-        Text("Hey, World!")
+       
+        ZStack {
+
+        Image("BACKGROUND")
+            .resizable()
+            Text(thker)
+            .font(.largeTitle)
+                .onTapGesture {
+                    self.thker = self.athkar.randomElement()!
+            }
+            
+            
+        VStack {
+            Image("Header")
+            .resizable()
+                .aspectRatio(contentMode: .fit)
+                Spacer()
+          
+            Image("Mosque")
+            .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+        
+        }
     }
-}
+        .edgesIgnoringSafeArea(.all)
+}}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
